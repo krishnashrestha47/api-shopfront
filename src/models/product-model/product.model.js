07330439d56762007878e90830436814a8fa3ccd
Krishna Shrestha <krish_sth2005@yahoo.com>
2022-08-30 21:10:44 +1000

    query to receive IDs and return products accordingly

--- src/models/product-model/product.model.js
+++ src/models/product-model/product.model.js
@@ -3,3 +3,11 @@ import productSchema from "./product.schema.js";
 export const getAllProducts = (filter) => {
   return productSchema.find(filter);
 };
+
+export const getProductsByCategory = (ids) => {
+  return productSchema.find({
+    catId: {
+      $in: ids,
+    },
+  });
+};
